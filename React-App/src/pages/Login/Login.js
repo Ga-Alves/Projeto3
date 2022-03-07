8e6def0ec15e581002c5598daaa8c04777255e20
MarcusFVilela <marcus.vilela@avenuecode.com>
2022-03-07 19:37:52 -0300

    login funcionando

--- React-App/src/pages/Login/Login.js
+++ React-App/src/pages/Login/Login.js
@@ -1,27 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Link} from 'react-router-dom'
+import Logar from "../../Requests/Usuario/Logar";
 
 
 export default function Login(){
+    const [email, setEmail] = useState('') 
+    const [senha, setSenha] = useState('') 
+
+    const handleClick = () => {
+        const obj = {
+            email, 
+            senha,
+        }         
+
+        Logar(obj);
+    }
+
+    const handleOnChangeEmail = (e) => {    
+        setEmail(e.target.value)
+      }
+    
+      const handleOnChangeSenha = (e) => {    
+        setSenha(e.target.value)
+      }
+
     return(
         <div id="Pg1" class ="d-flex justify-content-center align-items-center" >
             <div class="d-flex flex-column mb-1">
 
             <div class="p-2">
                 <label for="email" className="label-Login">Email</label>
-                <input type="email" placeholder="" className="input-Login"></input>
+                <input type="email" placeholder="" value={email} onChange={handleOnChangeEmail} className="input-Login"></input>
             </div>
 
             <div class="p-2">
                 <label for="senha" className="label-Login">Senha</label>
-                <input type="password" placeholder="" className="input-Login"></input>
+                <input type="password" placeholder="" value={senha} onChange={handleOnChangeSenha} className="input-Login"></input>
             </div>
 
             <div class="d-flex justify-content-center">
-                <button className="login-btn">Entrar</button>
+                <button className="login-btn" onClick={() => handleClick()}>Entrar</button>
             </div>
 
             <div class="d-flex p-2 justify-content-center">
